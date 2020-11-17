6bdae6903eb749c455928be2b937a887a59c1ef4
Renan Barbosa <renan_barbosa91@hotmail.com>
2020-11-17 11:21:21 -0300

    Welcome is a component now

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-var usuario = "Renan";
+function Welcome(propriedade) {
+  return (
+    <h1>
+      Oi, <span id="usuario">{propriedade.nome}</span>
+    </h1>
+  );
+}
+const element_welcome = <Welcome nome="Renan" />;
+ReactDOM.render(element_welcome, document.getElementById("welcome"));
 
 function tick() {
-  const element = (
+  const element_clock = (
     <div>
-      <h1>Oi, <span id='user'>{usuario}</span>!</h1>
       <h2>{new Date().toLocaleTimeString()}</h2>
     </div>
   );
-
-  ReactDOM.render(element, document.getElementById("root"));
+  ReactDOM.render(element_clock, document.getElementById("time"));
 }
 
 setInterval(tick, 1000);
