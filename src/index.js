21f13bee27764a12e267ca5d84dc8ae4836fd622
Renan Barbosa <renan_barbosa91@hotmail.com>
2020-11-18 13:53:36 -0300

    Functions became Class Components, added day of the week

--- src/index.js
+++ src/index.js
@@ -2,30 +2,74 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Welcome(propriedade) {
-  return (
-    <h1>
-      Oi, <span id="usuario">{propriedade.nome}</span>
-    </h1>
-  );
-}
-const element_welcome = <Welcome nome="Renan" />;
-ReactDOM.render(element_welcome, document.getElementById("welcome"));
-
-function tick() {
-  const element_clock = (
-    <div>
-      <h2>{new Date().toLocaleTimeString()}</h2>
-    </div>
-      );
-  ReactDOM.render(element_clock, document.getElementById("time"));
-
-  const element_date = (
-    <div>
-      <h2>{new Date().toLocaleDateString()}</h2>
-    </div>
-      );
-  ReactDOM.render(element_date, document.getElementById("date"));
+class Welcome extends React.Component {
+  render() {
+    return (
+      <h1>
+        Oi, <span id="usuario">{this.props.nome}</span>
+      </h1>
+    );
+  }
 }
 
-setInterval(tick, 1000);
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      data: new Date(),
+    });
+  }
+
+  render() {
+    var day = this.state.data.getDay();
+    var dayString;
+    switch (day) {
+      case 1:
+        dayString = "Segunda";
+        break;
+      case 2:
+        dayString = "Terça";
+        break;
+      case 3:
+        dayString = "Quarta";
+        break;
+      case 4:
+        dayString = "Quinta";
+        break;
+      case 5:
+        dayString = "Sexta";
+        break;
+      case 6:
+        dayString = "Sábado";
+        break;
+      default:
+        dayString = "Domingo";
+        break;
+    }
+
+    return (
+      <div>
+        <h2 id="hora">{this.state.data.toLocaleTimeString()}</h2>
+        <h2 id="data">
+          {dayString + ", " + this.state.data.toLocaleDateString()}
+        </h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Welcome nome={"Renan"} />, document.getElementById("welcome"));
+
+ReactDOM.render(<Clock />, document.getElementById("clock"));
